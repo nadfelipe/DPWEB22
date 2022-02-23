ef200c9725c80a79380eef6eb61d2e7636583efb
Daniel Felipe <nadfelipe.s@gmail.com>
2022-02-23 12:09:29 -0300

    up to date

--- M18/Daniel/JS - Aula 03/script.js
+++ M18/Daniel/JS - Aula 03/script.js
@@ -1,19 +1,40 @@
-
+//O elemento input_numero1
+const inputNum1 = document.getElementById("input_numero1");
+//O elemento input_numero2
+const inputNum2 = document.getElementById("input_numero2");
+//O elemento input_resultado
+const inputResultado = document.getElementById("input_resultado");
 
 function somarNumeros() {
-    var num1 = Number(document.getElementById("input_numero1").value);//valor do input 1
-    var num2 = Number(document.getElementById("input_numero2").value);//valor do input 1
+    //resultado da soma entre input 1 e 2
+    let resultadoSoma = Number(inputNum1.value) + Number(inputNum2.value);
 
-    var resultadoSoma = num1 + num2;//resultado da soma entre input 1 e 2
-
-    document.getElementById("input_resultado").value = resultadoSoma;
+    inputResultado.value = resultadoSoma;
 }
 
 function subtrairNumeros() {
-    var num1 =  Number(document.getElementById("input_numero1").value);//valor do input 1
-    var num2 =  Number(document.getElementById("input_numero2").value);//valor do input 1
+    //resultado da subtração entre input 1 e 2
+    let resultadoSubtracao = Number(inputNum1.value) - Number(inputNum2.value);
 
-    var resultadoSubtracao = num1 - num2;//resultado da subtração entre input 1 e 2
+    inputResultado.value = resultadoSubtracao;
+}
 
-    document.getElementById("input_resultado").value = resultadoSubtracao
+function multiplicarNumeros() {
+    //resultado da multiplicação entre input 1 e 2
+    let resultadoSoma = Number(inputNum1.value) * Number(inputNum2.value);
+
+    inputResultado.value = resultadoSoma;
+}
+
+function dividirNumeros() {
+    //resultado da divisão entre input 1 e 2
+    let resultadoSubtracao = Number(inputNum1.value) / Number(inputNum2.value);
+
+    inputResultado.value = resultadoSubtracao;
+}
+
+function limparInputs() {
+    inputNum1.value = '';
+    inputNum2.value = '';
+    inputResultado.value = '';
 }
---
+++ M18/Daniel/JS - Aula 04 - FestivalBotoes/script.js
@@ -0,0 +1,4 @@
+//Botão 2 - Muda o tamanho fonte
+function mudarTamanhoFonte() {
+    document.getElementById("secao_paragrafos").style.fontSize = "30px";
+}
