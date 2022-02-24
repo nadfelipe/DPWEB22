00c4fb605e61c07054d7b4ce2fba0c2d4f36974c
Daniel Felipe <nadfelipe.s@gmail.com>
2022-02-24 08:29:19 -0300

    up to date

--- T19/Daniel/JS - Aula 03/script.js
+++ T19/Daniel/JS - Aula 03/script.js
@@ -1,20 +1,40 @@
-//valor do input 1
-var num1 = Number(document.getElementById("input_numero1").value);
-//valor do input 2
-var num2 = Number(document.getElementById("input_numero2").value);
+//O elemento input_numero1
+const inputNumero1 = document.getElementById("input_numero1");
+//O elemento input_numero2
+const inputNumero2 = document.getElementById("input_numero2");
+//O elemento input_resultado
+const inputResultado = document.getElementById("input_resultado");
 
 function somarNumeros() {
     //efetuando a soma dos dois números que vieram do input
-    var resultadoSoma = num1 + num2;
-
+    let resultadoSoma = Number(inputNumero1.value) + Number(inputNumero2.value);
     //atribuindo a variável resultadoSoma para o input com id "input_resultado"
-    document.getElementById("input_resultado").value = resultadoSoma;
+    inputResultado.value = resultadoSoma;
 }
 
 function subtrairNumeros() {
     //efetuando a subtracao dos dois números que vieram do input
-    var resultadoSubtracao = num1 - num2;
-    
+    let resultadoSubtracao = Number(inputNumero1.value) - Number(inputNumero2.value);
     //atribuindo a variável resultadoSubtracao para o input com id "input_resultado"
-    document.getElementById("input_resultado").value = resultadoSubtracao;
+    inputResultado.value = resultadoSubtracao;
+}
+
+function multiplicarNumeros() {
+    //efetuando a multiplicacao dos dois números que vieram do input
+    let resultadoMultiplicacao = Number(inputNumero1.value) * Number(inputNumero2.value);
+    //atribuindo a variável resultadoSoma para o input com id "input_resultado"
+    inputResultado.value = resultadoMultiplicacao;
+}
+
+function dividirNumeros() {
+    //efetuando a divisão dos dois números que vieram do input
+    let resultadoDivisao = Number(inputNumero1.value) / Number(inputNumero2.value);
+    //atribuindo a variável resultadoSubtracao para o input com id "input_resultado"
+    inputResultado.value = resultadoDivisao;
+}
+
+function limparInputs() {
+    inputNumero1.value = '';
+    inputNumero2.value = '';
+    inputResultado.value = '';
 }
---
+++ T19/Daniel/JS - Aula 04 - FestivalBotoes/script.js
@@ -0,0 +1,16 @@
+const secaoParagrafos = document.getElementById('secao_paragrafos');
+
+// Botão 2 - Muda o tamanho da fonte dos parágrafos que estão na seção paragrafos
+function mudarTamanhoFonte() {
+    secaoParagrafos.style.fontSize = "30px";
+}
+
+// Botão 3 - Altera a cor da fonte dos parágrafos que estão na seção paragrafos
+function mudarCorFonte() {
+    secaoParagrafos.style.color = "#fff";
+}
+
+// Botão 4 - Altera a cor do fundo da div secao_paragrafos
+function mudarCorFundo() {
+    secaoParagrafos.style.backgroundColor = "#000";
+}
