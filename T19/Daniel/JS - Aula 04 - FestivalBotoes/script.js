cd05e240999785ad4258394149ad1008ff1ffb5b
Daniel Felipe <nadfelipe.s@gmail.com>
2022-02-25 16:57:45 -0300

    up to date

--- T19/Daniel/JS - Aula 04 - FestivalBotoes/script.js
+++ T19/Daniel/JS - Aula 04 - FestivalBotoes/script.js
@@ -64,8 +64,36 @@ function mudarCorFundoParagrafo() {
 
 // Botão 13 - Muda a cor do fundo de um único botão
 function mudarCorFundoBotao() {
-    document.getElementsByTagName("button")[5].style.backgroundColor = "lightblue";
+    // limiteBotoes é o índice máximo da lista de botões
+    let limiteBotoes = document.getElementsByTagName("button").length - 1;
+
+    // gerar um índice aleatório entre 0 e o último índice da lista de botões (18)
+    // Math.round(Math.random() * (maximo - minimo) + minimo)
+    // parseInt(Math.random() * (maximo - minimo) + minimo)
+    let indiceAleatorio = Math.round(Math.random() * (limiteBotoes - 0) + 0);
+
+    document.getElementsByTagName("button")[indiceAleatorio].style.backgroundColor = "lightblue";
+
+    document.getElementsByTagName("h2")[2].innerHTML = indiceAleatorio + 1;
 }
+
+// Botão 13 ALTERADO MALUCO DOIDO - Muda a cor do fundo de um único botão
+// function mudarCorFundoBotao() {
+//     // limiteBotoes é o índice máximo da lista de botões
+//     let limiteBotoes = document.getElementsByTagName("button").length - 1;
+
+//     // gerar um índice aleatório entre 0 e o último índice da lista de botões (18)
+//     // Math.round(Math.random() * (maximo - minimo) + minimo)
+//     // parseInt(Math.random() * (maximo - minimo) + minimo)
+//     let indiceAleatorio = Math.round(Math.random() * (limiteBotoes - 0) + 0);
+
+//     let botaoAleatorio = document.getElementsByTagName("button")[indiceAleatorio]
+     
+//     botaoAleatorio.style.backgroundColor = "lightblue";
+
+//     document.getElementsByTagName("h2")[2].innerHTML = `${indiceAleatorio + 1} - ${botaoAleatorio.innerHTML}`;
+// }
+
 
 // Botão 14 - Muda a cor do fundo de todos os botões
 function mudarCorFundoBotoes() {
@@ -80,4 +108,48 @@ function mudarCorFundoBotoes() {
     for (let contador = 0; contador < botoes.length; contador++){
         botoes[contador].style.backgroundColor = "lightgreen";
     }
+}
+
+// Botão 15 - Pega a quantidade de botões e coloca o número dentro do conteúdo interno do primeiro h2
+function contarBotoes() {
+    let quantidadeBotoes = document.getElementsByTagName("button").length;
+
+    document.getElementsByTagName("h2")[0].innerHTML = `Número de botões do site: ${quantidadeBotoes}`;
+}
+
+// Botão 16 - Muda o conteúdo interno do primeiro elemento que tem a classe paragrafo
+function alterarTextoParagrafo() {
+    document.querySelector(".paragrafo").innerHTML = "Texto alterado com query selector";
+}
+
+// Botão 17 - Muda a cor do fundo da página inteira
+function mudarCorPagina() {
+    document.querySelector("body").style.backgroundColor = "#ffff00";
+}
+
+// Botão 18 - Troca a imagem do laptop da xuxa pela imagem do outro pc
+function mudarImagem() {
+    document.querySelector("img").src = "./img/alien.jpg";
+    document.querySelector("img").alt = "Imagem de um notebook gamer";
+}
+
+// Botão 19 - Aciona (quase) todas as funções
+function acionarFuncoes() {
+    //Não vamos colocar os botões 1, 8, 9, 10 e 13*
+    
+    mudarTamanhoFonte(); // Função botão 2
+    mudarCorFonte(); // Função botão 3
+    mudarCorFundo(); // Função botão 4
+    mudarFonteItalico(); // Função botão 5
+    mudarFonteNegrito(); // Função botão 6
+    adicionarBorda(); // Função botão 7
+
+    mudarSegundoTexto(); // Função botão 11
+    mudarCorFundoParagrafo(); // Função botão 12
+
+    mudarCorFundoBotoes(); // Função botão 14
+    contarBotoes(); // Função botão 15
+    alterarTextoParagrafo(); // Função botão 16
+    mudarCorPagina(); // Função botão 17
+    mudarImagem(); // Função botão 18   
 }
